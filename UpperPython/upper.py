131d3544add139dfb31c39b3e91a1852cad2c260
des10sjones <143039242+des10sjones@users.noreply.github.com>
2024-07-09 16:53:18 +0000

    **

---
+++ UpperPython/upper.py
@@ -0,0 +1,11 @@
+# Function to convert lowercase to uppercase
+def convert_to_uppercase(input_string):
+    return input_string.upper()
+
+# Example usage
+input_string = input("Please enter a string: ")
+
+# Convert the string to uppercase
+uppercase_string = convert_to_uppercase(input_string)
+print("Original string:", input_string)
+print("Uppercase string:", uppercase_string)
